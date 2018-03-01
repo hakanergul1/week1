3bae323c57f5cc0783625af4c9689d130273bb92
hakanergul1 <36765515+hakanergul1@users.noreply.github.com>
2018-03-01 14:15:50 +0300

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,23 @@
+# week1
+for x in range(3):
+        sayi1=int(input("sayi gir : "))
+        sayi2=int(input("sayi gir : "))
+        s=0
+        print("Toplama Islemi(+)")
+        print("Cikarma Islemi(-)")
+        print("Bolme   Islemi(/)")
+        print("Carpma  Islemi(*)")
+        islem=input("islem : ")
+        if islem == "+" :
+            s=sayi1+sayi2
+
+            print("sonuc", s)
+        elif islem=="-":
+            s=sayi1-sayi2
+            print("sonuc",s)
+        elif islem=="*":
+            s=sayi1*sayi2
+            print("sonuc",s)
+        elif islem=="/":
+            s=sayi1/sayi2
+            print("sonuc",s)
