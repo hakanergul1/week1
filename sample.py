fe67ce792570d799082d80aa0c6f28408a553576
hakanergul1 <36765515+hakanergul1@users.noreply.github.com>
2018-03-02 16:38:55 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,23 +1,27 @@
-# week1
-for x in range(3):
-        sayi1=int(input("sayi gir : "))
-        sayi2=int(input("sayi gir : "))
-        s=0
-        print("Toplama Islemi(+)")
-        print("Cikarma Islemi(-)")
-        print("Bolme   Islemi(*)")
-        print("Carpma  Islemi(/)")
-        islem=input("islem : ")
-        if islem == "+" :
-            s=sayi1+sayi2
+f = int(input("İşlem Kaç Defa Tekrar Etsin : "))
+if f<=0:
+    print ("Döngü sıfırdan tekrar edemez. : ")
+for i in range (0,f) :
+    sayi1 = int(input("1. Sayıyı Girin :"))
+    sayi2 = int(input("2. Sayıyı Girin :"))
+    islem = input("İşlem Operatörü Seçiniz (+,-,*,/):")
 
-            print("sonuc", s)
-        elif islem=="-":
-            s=sayi1-sayi2
-            print("sonuc",s)
-        elif islem=="*":
-            s=sayi1*sayi2
-            print("sonuc",s)
-        elif islem=="/":
-            s=sayi1/sayi2
-            print("sonuc",s)
+
+    if islem == "+" :
+        print (sayi1+sayi2)
+
+    elif islem == "-" :
+        print (sayi1-sayi2)
+
+    elif islem == "*" :
+        print (sayi1*sayi2)
+
+    elif b==0 :
+        print ("Sıfır ile bölme işlemi yapamazsınız.")
+    elif islem =="/" :
+        print (sayi1/sayi2)
+
+    elif islem == "exit":
+        break
+    else:
+        print("İşlem operatörü giriniz")
